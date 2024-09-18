f22e7298dca9ec74ae404f898e25a1c296edff3a
Timmy Jose <zoltan.jose@gmail.com>
2024-09-18 22:13:20 +0530

    feat: custom hook - useOnlineStatus.

--- src/pages/CustomHookDemo.tsx
+++ src/pages/CustomHookDemo.tsx
@@ -1,15 +1,20 @@
 import { useNavigate } from "react-router-dom"
+import { globalStyles } from "../globalStyles"
+import { useOnlineStatus } from "../hooks/useOnlineStatus"
 
 const CustomHookDemo = () => {
   const navigate = useNavigate()
+  
+  const isOnline = useOnlineStatus()
 
   return (
-    <>
+    <div style={globalStyles.container}>
       <h1>Custom Hook Demo</h1>
+      <p>Online ? {isOnline ? '\u2705' : '\u274c'}</p>
       <button onClick={() => navigate('/')}
         >Home
       </button>
-    </>
+    </div>
   )
 }
 
